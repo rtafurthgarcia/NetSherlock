04276d12602e0260c7cf0aa97429fb34697e548a
rtafurthgarcia <r.tafurthgarcia@hftm.ch>
2023-12-06 20:51:29 +0100

    Some basic skeleton of the app Just includes a basic navigation drawer

---
+++ android/app/src/main/kotlin/com/example/netsherlock/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.netsherlock
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/navigationdrawer.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+
+class DrawerDestination {
+  const DrawerDestination(this.label, this.icon, this.selectedIcon);
+
+  final String label;
+  final Widget icon;
+  final Widget selectedIcon;
+}
+
+const List<DrawerDestination> destinations = <DrawerDestination>[
+  DrawerDestination('Profile', Icon(Icons.account_circle_outlined), Icon(Icons.account_circle)),
+  DrawerDestination('Alerts', Icon(Icons.warning_amber_outlined), Icon(Icons.warning_amber)),
+  DrawerDestination('Scans', Icon(Icons.domain_verification_outlined), Icon(Icons.domain_verification)),
+  DrawerDestination('DNS', Icon(Icons.dns_outlined), Icon(Icons.dns)),
+];
+
+class AppNavigationDrawer extends StatefulWidget {
+  const AppNavigationDrawer({super.key});
+
+  @override
+  State<AppNavigationDrawer> createState() => AppNavigationDrawerState();
+}
+
+class AppNavigationDrawerState extends State<AppNavigationDrawer> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  int screenIndex = 0;
+  late bool showNavigationDrawer;
+
+  void handleScreenChanged(int selectedScreen) {
+    setState(() {
+      screenIndex = selectedScreen;
+    });
+  }
+
+  void openDrawer() {
+    scaffoldKey.currentState!.openEndDrawer();
+  }
+
+  Widget buildBottomBarScaffold() {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text('Page Index = $screenIndex'),
+          ],
+        ),
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: screenIndex,
+        onDestinationSelected: (int index) {
+          setState(() {
+            screenIndex = index;
+          });
+        },
+        destinations: destinations.map(
+          (DrawerDestination destination) {
+            return NavigationDestination(
+              label: destination.label,
+              icon: destination.icon,
+              selectedIcon: destination.selectedIcon,
+              tooltip: destination.label,
+            );
+          },
+        ).toList(),
+      ),
+    );
+  }
+
+  Widget buildDrawerScaffold(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      body: SafeArea(
+        bottom: false,
+        top: false,
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: NavigationRail(
+                minWidth: 50,
+                destinations: destinations.map(
+                  (DrawerDestination destination) {
+                    return NavigationRailDestination(
+                      label: Text(destination.label),
+                      icon: destination.icon,
+                      selectedIcon: destination.selectedIcon,
+                    );
+                  },
+                ).toList(),
+                selectedIndex: screenIndex,
+                useIndicator: true,
+                onDestinationSelected: (int index) {
+                  setState(() {
+                    screenIndex = index;
+                  });
+                },
+              ),
+            ),
+            const VerticalDivider(thickness: 1, width: 1),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text('Page Index = $screenIndex'),
+                  ElevatedButton(
+                    onPressed: openDrawer,
+                    child: const Text('Open Drawer'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      endDrawer: NavigationDrawer(
+        onDestinationSelected: handleScreenChanged,
+        selectedIndex: screenIndex,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(28, 16, 16, 10),
+            child: Text(
+              'NetSherlock',
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+          ),
+          ...destinations.map(
+            (DrawerDestination destination) {
+              return NavigationDrawerDestination(
+                label: Text(destination.label),
+                icon: destination.icon,
+                selectedIcon: destination.selectedIcon,
+              );
+            },
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(28, 16, 28, 10),
+            child: Divider(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    showNavigationDrawer = MediaQuery.of(context).size.width >= 450;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return showNavigationDrawer
+        ? buildDrawerScaffold(context)
+        : buildBottomBarScaffold();
+  }
+}

96ba4239c63d23f76e65aeb8b1548bf602f31fba
rtafurthgarcia <r.tafurthgarcia@hftm.ch>
2023-12-19 21:23:30 +0100

    Set a minimum window size and renamed the package

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:netsherlock/services/shodan_account_service.dart';
 import 'package:netsherlock/widgets/navigationdrawer_widget.dart';
 import 'package:provider/provider.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+Future<void> main() async {
+WidgetsFlutterBinding.ensureInitialized();
+  // Must add this line.
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = const WindowOptions(
+    minimumSize: Size(400, 320),
+    center: true,
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    titleBarStyle: TitleBarStyle.normal,
+    windowButtonVisibility: true
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
   runApp(const MainApp());
 }
 
